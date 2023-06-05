9cd5b7d6503db22a74a24b8a441b48e29904406c
jo <jonathan_92222@hotmail.com>
2023-06-05 19:20:49 -0400

    commit

--- cljs-runtime/cljs_playground.core.js
+++ cljs-runtime/cljs_playground.core.js
@@ -1,8 +1,28 @@
 goog.provide('cljs_playground.core');
 var module$node_modules$$logseq$libs$dist$lsplugin_user=shadow.js.require("module$node_modules$$logseq$libs$dist$lsplugin_user", {});
+var G__38284_38288 = Thread.sleep((4000));
+var G__38285_38289 = cljs_playground.core.create_garden_calendar = (function cljs_playground$core$create_garden_calendar(){
+return promesa.protocols._mcat(promesa.protocols._promise(null),(function (___33724__auto__){
+return promesa.protocols._mcat(promesa.protocols._promise(logseq.Editor.getCurrentBlock()),(function (current_block){
+return promesa.impl.resolved(null);
+}));
+}));
+});
+(cljs_playground.core.future.cljs$core$IFn$_invoke$arity$2 ? cljs_playground.core.future.cljs$core$IFn$_invoke$arity$2(G__38284_38288,G__38285_38289) : cljs_playground.core.future.call(null,G__38284_38288,G__38285_38289));
+cljs_playground.core.slashcommands = (function cljs_playground$core$slashcommands(){
+cljs_playground.core.regSlashCmd = logseq.Editor.registerSlashCommand;
+
+var G__38286 = "create-garden-calendar";
+var G__38287 = (function (){
+return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["ok3"], 0));
+});
+return (cljs_playground.core.regSlashCmd.cljs$core$IFn$_invoke$arity$2 ? cljs_playground.core.regSlashCmd.cljs$core$IFn$_invoke$arity$2(G__38286,G__38287) : cljs_playground.core.regSlashCmd.call(null,G__38286,G__38287));
+});
 logseq.App.showMsg("Gardening rocks!");
 cljs_playground.core.main = (function cljs_playground$core$main(){
-return logseq.App.showMsg("Gardening rocks!");
+logseq.App.showMsg("Gardening rocks!");
+
+return cljs_playground.core.slashcommands();
 });
 cljs_playground.core.init = (function cljs_playground$core$init(){
 return logseq.ready(cljs_playground.core.main).catch(console.error);
--- src/main/cljs_playground/core.cljs
+++ src/main/cljs_playground/core.cljs
@@ -6,32 +6,28 @@
 
             [promesa.core :as p]))
 
-;; (future (Thread/sleep 4000)
+(future (Thread/sleep 4000)
         ;; (println "I'll print after 4 seconds"))
 
-;; (defn create-garden-calendar []
-;;   (p/let [current-block (js/logseq.Editor.getCurrentBlock)]
-;;   )
+(defn create-garden-calendar []
+  (p/let [current-block (js/logseq.Editor.getCurrentBlock)]
+  )
 
-;;   )
-;; (def x (create-garden-calendar))
+  )
+)
 
-;; (p/then x (println p/extract x))
+(defn slashcommands []
+  (def regSlashCmd js/logseq.Editor.registerSlashCommand)
+  (regSlashCmd
+   "create-garden-calendar"
+   #(println "ok3")))
 
-;; (def x1 (int-array '(1 2 3)))
-;; (println (aget x1 2))
-
-;; (defn slashcommands [])(
-;;   (def regSlashCmd js/logseq.Editor.registerSlashCommand)
-;;   (regSlashCmd
-;;    "create-garden-calendar"
-;;    #(println "ok3")))
 
 (js/logseq.App.showMsg "Gardening rocks!")
 
 (defn main []
   (js/logseq.App.showMsg "Gardening rocks!")
-  ;; (slashcommands)
+  (slashcommands)
   )
 (defn init []
   (-> (js/logseq.ready main)
